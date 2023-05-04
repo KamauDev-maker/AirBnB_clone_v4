0bc2dc37698da9fa425cfb16c401db82e300919d
KamauDev-maker <oscarnjenga@gmail.com>
2023-05-04 19:37:58 +0000

    file path corrections

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    const amenity_ids = [];
+    $('.amenity-checkbox').on('change', function() {
+        const amenity_id = $(this).parent().data('id');
+        const amenityName = $(this).parent().data('name');
+        if ($(this).is('checked')) {
+            amenity_ids.push(amenity_id);
+        } else {
+            const index = amenity_ids.indexOf(amenity_id);
+            if (index !== -1) {
+                amenity_ids.splice(index, 1);
+            }
+        }
+
+        if (amenity_ids.length === 0) {
+            $('.amenities h4').html('&nbsp;');
+        } else {
+            const amenityNames = amenity_ids.map(id => {
+                return $('.amenities-list li[data-id="' + id + '"]').data('na
+me');
+            });
+            $('.amenities h4').text(amenityNames.join(', '));
+        }
+    });
+});
