233f028b4b8de888014f3818a9bb54f555cd0096
KamauDev-maker <oscarnjenga@gmail.com>
2023-05-04 20:38:22 +0000

    the API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    const amenity_ids = [];
+    $('.amenity-checkbox').on('change', function() {
+        const amenity_id = $(this).parent().data('id');
+        const amenityName = $(this).parent().data('name');
+        if ($(this).is('checked')) {
+            amenity_ids.push(amenity_id);
+        } else {
+            const index = amenity_ids.indexOf(amenity_id);
+            if (index !== -1) {
+                amenity_ids.splice(index, 1);
+            }
+        }
+
+        if (amenity_ids.length === 0) {
+            $('.amenities h4').html('&nbsp;');
+        } else {
+            const amenityNames = amenity_ids.map(id => {
+                return $('.amenities-list li[data-id="' + id + '"]').data('na
+me');
+            });
+            $('.amenities h4').text(amenityNames.join(', '));
+            $.get("http://0.0.0.0:5001/api/v1/status/", function(data) {
+                if (data.status === 'OK') {
+                    $('#api_status').addClass('available');
+                } else {
+                    $('#api_status').removeClass('available');
+                }
+            }
+        }
+    });
+});
